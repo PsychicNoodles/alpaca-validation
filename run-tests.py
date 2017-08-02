8402b8c12cd31f396aa763ccce1589542e063b2f
Sara Marku <markusar@grinnell.edu>
2017-08-02 13:36:23 -0500

    adding printf tests and removing emulatedtls and static libgcc flags

--- run-tests.py
+++ run-tests.py
@@ -3,9 +3,11 @@ from subprocess import PIPE
 import sys
 import os
 
+NUM_PRINTFS = 100
+
 TEST_PROG = "test-suite"
-tests = ["bool", "short", "int", "long", "long_long", "float", "double", "int_struct", "double_struct", "two_int", "two_float", "mixed", "eight_int", "everything", "pointer", "malloc_free", "triple_double", "param_float", "local", "mmap", "sixteen_ll", "mem_cpy", "global_write"]
-results = ["1", "2", "3", "4", "5", "-12345.6", "7.0", "8", "9.0", "10 11", "12.0 13.0", "14 15 16.0", "17 18 19 20 21 22 23 24", "1 24 25 26 27 28.0 29.0", "30 31.0 testtest", "0", "32.0 33.0 34.0", "9.3", "2", "1234", "123456 234567 345678 456789 5678910 67891011 789101112 8910111213 91011121314 101112131415 111213141516 121314151617 131415161718 141516171819 151617181920 161718192021", "abcdefghijklmnopqrstuvwxyz", "1 2.0 testtest"]
+tests = ["bool", "short", "int", "long", "long_long", "float", "double", "int_struct", "double_struct", "two_int", "two_float", "mixed", "eight_int", "everything", "pointer", "malloc_free", "triple_double", "param_float", "local", "mmap", "sixteen_ll", "mem_cpy", "global_write", "printf"]
+results = ["1", "2", "3", "4", "5", "-12345.6", "7.0", "8", "9.0", "10 11", "12.0 13.0", "14 15 16.0", "17 18 19 20 21 22 23 24", "1 24 25 26 27 28.0 29.0", "30 31.0 testtest", "0", "32.0 33.0 34.0", "9.3", "2", "1234", "123456 234567 345678 456789 5678910 67891011 789101112 8910111213 91011121314 101112131415 111213141516 121314151617 131415161718 141516171819 151617181920 161718192021", "abcdefghijklmnopqrstuvwxyz", "1 2.0 testtest", "\n".join(["printf %s" % num for num in range(NUM_PRINTFS)])]
 
 os.environ["LD_PRELOAD"] = "./alpaca.so:libelf++.so:libudis86.so"
 
--- test-suite.c
+++ test-suite.c
@@ -198,7 +198,7 @@ pointer_t pointer_func() {
   return ret;
 }
 
-#define NUM_MALLOCS 100
+#define NUM_MALLOCS 1
 void malloc_free_func() {
   char* a[NUM_MALLOCS];
   printf("malloced array of size %d\n", NUM_MALLOCS);
@@ -208,14 +208,16 @@ void malloc_free_func() {
   }
   printf("finished mallocing\n");
 
+  /**
   for(int i = 0; i < NUM_MALLOCS; i++)
-   printf("malloced: %p\n", a[i]);
+    printf("malloced: %p\n", a[i]);
 
   for (int i = 0; i < NUM_MALLOCS; i++) {
    free(a[i]);
    printf("freed item at %d\n", i);
   }
   printf("returning\n");
+  **/
 }
 
 triple_double_t triple_double_func() {
